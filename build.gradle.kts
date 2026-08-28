9723392c33210dbed04c68b52a5b6a653007a039
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-08-28 03:07:09 +0300

    feat: init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.4.0"
+}
+
+group = "org.scobca"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
